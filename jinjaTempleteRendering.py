4259b2d1a66d29f6296e6d65859f6eb0e4e347c3
Wojciech Gryglas <wgryglas@meil.pw.edu.pl>
2019-09-20 13:02:02 +0200

    testing define

--- jinjaTempleteRendering.py
+++ jinjaTempleteRendering.py
@@ -1,7 +1,9 @@
-from jinja2 import Environment, FileSystemLoader, select_autoescape
+from jinja2 import Environment, FileSystemLoader
 
-env = Environment(loader = FileSystemLoader('jinja_templates'),
-                  autoescape=select_autoescape(['html', 'xml']),
+# from jinja2 import select_autoescape
+
+env = Environment(loader=FileSystemLoader('jinja_templates'),
+                  # autoescape=select_autoescape(['html', 'xml']),
                   lstrip_blocks=True,
                   trim_blocks=True)
 
@@ -13,8 +15,31 @@ class P:
         self.text = text
 
 
+class Input:
+    def __init__(self, value):
+        self.value = value
+
+    @property
+    def text(self):
+        return str(self.value)
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Vector:
+    def __init__(self, x, y, z):
+        self.x = Input(x)
+        self.y = Input(y)
+        self.z = Input(z)
+
+
 paragraphs = [P("test"), P("test2"), P("test3")]
 
-doc = template.render(paragraphs = paragraphs)
+# doc = template.render(paragraphs=paragraphs)
+# print doc
 
-print doc
+def_vector = env.get_template("define_vector.html")
+print def_vector.render(content=Vector(1, 2, 3))
+
+
